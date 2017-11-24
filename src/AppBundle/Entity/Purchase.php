36520ee5880512e121a4339f377148ae05e2436b
VytauteVita <vita.vytaute@gmail.com>
2017-11-24 23:37:45 +0200

    Created Entity Purchace (Order)

---
+++ src/AppBundle/Entity/Purchase.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Purchase
+ *
+ * @ORM\Table(name="purchase")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PurchaseRepository")
+ */
+class Purchase
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="state", type="string", length=32)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="post", type="string", length=255, nullable=true)
+     */
+    private $post;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fullfilledAt", type="datetime", nullable=true)
+     */
+    private $fullfilledAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Purchase
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set state
+     *
+     * @param string $state
+     *
+     * @return Purchase
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set post
+     *
+     * @param string $post
+     *
+     * @return Purchase
+     */
+    public function setPost($post)
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    /**
+     * Get post
+     *
+     * @return string
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /**
+     * Set fullfilledAt
+     *
+     * @param \DateTime $fullfilledAt
+     *
+     * @return Purchase
+     */
+    public function setFullfilledAt($fullfilledAt)
+    {
+        $this->fullfilledAt = $fullfilledAt;
+
+        return $this;
+    }
+
+    /**
+     * Get fullfilledAt
+     *
+     * @return \DateTime
+     */
+    public function getFullfilledAt()
+    {
+        return $this->fullfilledAt;
+    }
+}
+
